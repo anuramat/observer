68d634f3412a973fc8c629333decac78fc9e13ec
Arsen Nuramatov <x@ctrl.sn>
2025-08-22 01:37:18 +0200

    add proof of concept

--- observer.sh
+++ observer.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-rect=$(swaymsg -t get_tree | jq -r '..|select(.focused? == true) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
-grim -g "$rect" ./screenshot.png
+file=$(mktemp --suffix=.png)
+while true; do
+	rect=$(swaymsg -t get_tree | jq -r '..|select(.focused? == true) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
+	grim -g "$rect" "$file"
+	mods --no-cache -q -i "$file" -a llama-cpp -m dummy 'describe the screenshot in three sentences: what is the user doing right now?'
+	sleep 1
+done
